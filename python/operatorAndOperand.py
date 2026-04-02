142e10a8ef911dfbf8c4694237e7e677a27f53ce
Ab-Tayyab <abdullahtayyab414@gmail.com>
2026-04-02 12:56:36 +0500

    Day 2

---
+++ python/operatorAndOperand.py
@@ -0,0 +1,96 @@
+# Arithmatic operator s
+
+# addition 
+print(2+2)
+# Subtraction 
+print(2-2)
+# Multiplication 
+print(2*2)
+# Division 
+print(2/2)
+# Exponentiaton or 2 ki power 2 or 2 ki power 3
+print(2**3)
+# Modulus 
+print(2%2)
+# floor division or show value before fullstop like 2.34 show just 2
+print(2//2)
+
+# comparison operators
+
+# Equal to 
+print(2==2)
+# less then equal to 
+print(2<=2)
+# greater then equal to 
+print(2>=2)
+# not equal to 
+print(2!=2)
+# less then 
+print(2<2)
+# greater then 
+print(2>2)
+
+# Logical operators 
+
+# AND operator 
+print(3>2 and 2<7)
+# OR operator 
+print(2<4 or 2>3)
+# Not operator it convert true to false or false to true
+print(not(3>2 and 2<7))
+
+# Assignment operators 
+
+# equal to 
+x=10
+print(x)
+# plus equal to 
+x+=10
+print(x)
+# minus equal to 
+x-=10
+print(x)
+# multiply equal to 
+x*=10
+print(x)
+
+# Identity operator 
+# used to determine if two variables point to the exact same object in memory. They differ from equality operators (== and !=), which only check if the values of the objects are the same
+
+# is operator
+ 
+a = 123
+b="123"
+print (a is b)
+
+# is not operator
+
+a = 123
+b="123"
+print(a is not b)
+
+# Bitwise Operator , use to comapre by 1 0 value
+
+# find 1 0 value 
+
+print(bin(10))
+
+# AND operator 
+print(10 & 8)
+# OR operator 
+print(10 | 8)
+# XOR operator 
+print(10^8)
+# zero fill left shift 
+print(10<<2)
+# zero fill right shift
+print(10>>2)
+
+# Membership operator, use to check presence of a sequence in object  
+
+# in operator 
+name = "Abdullah"
+print("d" in name)
+# not in operator 
+name = "Tayyab"
+print("d" not in name)
