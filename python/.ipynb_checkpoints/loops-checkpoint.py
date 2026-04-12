b179b9ae4e9ce0d0808e874367578b68611a1271
Ab-Tayyab <abdullahtayyab414@gmail.com>
2026-04-12 23:04:04 +0500

    day 12

---
+++ python/.ipynb_checkpoints/loops-checkpoint.py
@@ -0,0 +1,75 @@
+# for loop 
+# print table 
+
+# n = int(input("Enter number here!"))
+# for i in range(1,11):
+#     print(n," X ",i," = ",n*i)
+    
+# print even odd 
+# odd number
+ 
+# for i in range(1,11,2):
+#     print(i)
+    
+# even number 
+
+# for i in range(0,11,2):
+#     print(i)
+
+# while loop 
+# print table 
+
+# tableNumber=int(input("Enter number hera!"))
+# n=1
+# while n<=10:
+#     print(tableNumber," X ",n," = ",n*tableNumber)
+#     n+=1 
+
+# even odd 
+# even 
+
+# num = (int(input("Enter number! ")))
+# n=1
+# while n<=num:
+#     if(n%2==0):
+#         print(n)
+#     n+=1
+
+# odd 
+
+# num = (int(input("Enter number! ")))
+# n=1
+# while n<=num:
+#     if(n%2!=0):
+#         print(n)
+#     n+=1
+    
+# while true loop 
+
+# num = (int(input("Enter Table Number! ")))
+# n=1
+# while True:
+#     print(num," X ",n," = ", n*num)
+#     n+=1
+#     if(n>10):
+#         terminate=input("Enter Yes or No for terminate or continue! ")
+#         if terminate =="yes":
+#             break
+#         else:
+#             num = (int(input("Enter Table Number! ")))
+#             n=1
+            
+# nested loop 
+
+# 10 pushup with 3 set
+# for i in range(1,4):
+#     for j in range(1,11):
+#         print(j, end="") #end use for same line
+#     print() #new line         
+
+# number pattren with pyramid 
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j, end="")
+    print()
