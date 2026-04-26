1fa555cfcae9fa0eae333139a6f8983f4c16b1b2
Ab-Tayyab <abdullahtayyab414@gmail.com>
2026-04-26 11:03:56 +0500

    day 26

--- python/projects/chatRoom.py
+++ python/projects/chatRoom.py
@@ -1,42 +1,70 @@
-#  Create a Chat System using Object-Oriented Programming (OOP) concepts.
-# You need to create the following classes:
-# • User
-# • Message
-# • ChatRoom
-#  The system should implement the following functionalities:
-# • Sending messages
-# • Viewing chat history
-# • User joining and leaving the chat room
-
 from datetime import datetime
-
 class User:
-    def __init__(self,userName):
+    def __init__(self, userName):
         self.userName = userName
 
 
 class Message:
-    def __init__(self,sender,content):
+    def __init__(self, sender, content):
         self.sender = sender
-        self.content=content
+        self.content = content
+        self.time=datetime.now()
 
 class ChatRoom:
-    def __init__(self,roomName):
+    def __init__(self, roomName):
         self.roomName = roomName
         self.users = []
-        self.messages=[]
-    def addUser(self,user):
-        if len(self.users)<2:
+        self.messages = []
+
+    def addUser(self, user):
+        if len(self.users) < 2:
             self.users.append(user)
 
+    def addMessage(self, message):
+        self.messages.append(message)
+
+    def showHistory(self):
+        print("\n-----Chat History-----\n")
+        if not self.messages:
+            print("No Message yet!")
+
+        for msg in self.messages:
+            time_str = msg.time.strftime("%H:%M:%S")
+            print(f"[{time_str}]=> {msg.sender.userName}: {msg.content}")
+        print("\n----------------------\n")
+    def startChat(self):
+        if len(self.users) < 2:
+            print("Need 2 user to start chat!")
+            return
+        
+        print("\n-----------------------------------------")
+        print("Chat Started")
+        print("Press 0 for exit chat and return to menu:")
+        print("-----------------------------------------\n")
+
+        turn = 0
+        while True:
+            current_user = self.users[turn]
+            msg = input(f"{current_user.userName}: ")
+            if msg == "0":
+                print("\n--------")
+                print("Chat End")
+                print("--------\n")
+                break
+            message = Message(current_user, msg)
+            self.addMessage(message)
+            turn = 1 - turn
 
 
+# --------------- Main Program-------------------
 def main():
-    roomName = input("Enter chat room name: ")
+    print("\n----------------------")
+    roomName = input("Chat Room Name: ")
     chatRoom = ChatRoom(roomName)
 
-    sender1 = input("Enter name of user 1: ")
-    sender2 = input("Enter name of user 2: ")
+    sender1 = input("User 1: ")
+    sender2 = input("User 2: ")
+    print("----------------------\n")
     createUser1 = User(sender1)
     createUser2 = User(sender2)
 
@@ -44,21 +72,29 @@ def main():
     chatRoom.addUser(createUser2)
 
     while True:
-        print("-------Menu-------")
+        print("-------------")
+        print("Programm Menu")
+        print("-------------\n")
+    
         print("1. Start Chat")
         print("2. Show Chat History")
         print("0. Exit Program")
 
         n = int(input("Enter Choice: "))
 
-        if(n==1):
-            print("Start Chat")
-        elif n==2:
-            print("Chat history")
-        elif n==0:
+        if n == 1:
+            chatRoom.startChat()
+        elif n == 2:
+            chatRoom.showHistory()
+        elif n == 0:
+            print("\n------------")
             print("Programm end")
+            print("------------")
+
             break
         else:
             print("Invalid Choice")
+
+
 if __name__ == "__main__":
     main()
